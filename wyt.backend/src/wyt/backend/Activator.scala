9d7a5b3b214d6d4b97ffd1346aacf3187569feae
Carsten Gräf <carsten.graef@kvb.de>
2017-06-30 13:29:12 +0200

    initial

---
+++ wyt.backend/src/wyt/backend/Activator.scala
@@ -0,0 +1,25 @@
+package wyt.backend
+
+import akka.event.LogSource
+import akka.osgi.ActorSystemActivator
+import com.typesafe.config.Config
+import domino.DominoActivator
+import domino.capsule.Capsule
+import org.osgi.framework.BundleContext
+import io.skysail.core.app.ApplicationRoutesProvider
+import akka.actor.ActorSystem
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+import scala.concurrent.Future
+import domino.service_watching.ServiceWatcherEvent._
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.server.Directives._
+import org.slf4j.LoggerFactory
+
+class Activator extends DominoActivator {
+  
+}
